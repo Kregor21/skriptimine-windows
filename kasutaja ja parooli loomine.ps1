94af7f04b75430fe1d419d1b2a2433205bef4cce
kregortoimetaja <kregor.toimetaja@voco.ee>
2022-11-08 05:37:07 -0800

    kasutaja ja parooli loomine.ps1

---
+++ kasutaja ja parooli loomine.ps1
@@ -0,0 +1,4 @@
+﻿#Loome kasutajale parooli
+$kasutajaparool = ConvertTo-SecureString "Qwerty12" -ASPlainText -force
+#Loon Kasutaja
+New-LocalUser "kasutaja1" -Password $kasutajaparool -FullName "esimene kasutaja" -Description "Local account"
