883cec39b349b051cd5229a17e7ac801e9a63196
kregortoimetaja <kregor.toimetaja@voco.ee>
2022-11-09 01:20:28 -0800

    lokaalne kasutaja loomine parameetritega.ps1

---
+++ lokaalne kasutaja loomine parameetritega.ps1
@@ -0,0 +1,10 @@
+﻿#vajalikud parameetrid
+param(
+$kasutajanimi,
+$taisnimi,
+$kontokirjeldus
+)
+#loome kasutaja jaoks parooli
+$kasutajaparool = ConvertTo-SecureString "Qwerty12" -AsPlainText -Force
+# loome kasutaja
+New-LocalUser $kasutajanimi -Password $kasutajaparool -FullName $taisnimi -Description $kontokirjeldus
