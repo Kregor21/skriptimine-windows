b321c46e660460de51f02cd3bb2015ced86a1d04
kregortoimetaja <kregor.toimetaja@voco.ee>
2022-11-09 01:45:51 -0800

    kui skript l'heb valesti k'ima.ps1

---
+++ kui skript l'heb valesti k'ima.ps1
@@ -0,0 +1,19 @@
+﻿#vajalikud parameetrid
+param(
+$kasutajanimi,
+$taisnimi,
+$kontokirjeldus
+)
+echo $args
+#kontroll parameetri arvudele
+if($args.Count -ne 3){
+ echo ".\skriptifailinimi.ps1 kasutajanimi taisnimi kirjeldus"
+} else {
+        $kasutajanimi = $args[0]
+        $taisnimi = $args[1]
+        $kontokirjeldus = $args[2]
+#loome kasutaja jaoks parooli
+$kasutajaparool = ConvertTo-SecureString "Qwerty12" -AsPlainText -Force
+# loome kasutaja
+New-LocalUser $kasutajanimi -Password $kasutajaparool -FullName $taisnimi -Description $kontokirjeldus
+}
