bdea94a81fec1eaf3adbc0841c28158533955fd3
kregortoimetaja <kregor.toimetaja@voco.ee>
2022-11-16 06:28:04 -0800

    Lokaalse kasutaja loomine 2.ps1

---
+++ Lokaalse kasutaja loomine 2.ps1
@@ -0,0 +1,16 @@
+﻿#määrame kasutajale parool
+$Kasutajaparool = ConvertTo-SecureString 'Parool11!' -Asplaintext -force
+#read hostiga $eesnimi
+$Eesnimi = Read-host "Sisesta oma eesnimi"
+#read hostiga $perenimi
+$Perenimi = Read-Host "Sisesta oma perenimi"
+#$kasutajnimi sätestamine
+$KasutajaNimi = $Eesnimi.ToLower() + " " + $Perenimi.ToLower()
+#$täisnimi
+$TaisNimi =$Eesnimi + " " + $Perenimi
+#$kontokirjeldus
+$Kirjeldus = "Lokaalne konto"
+#kasutajanime confirmimine? write-outputiga
+Write-Output "Loodav kasutaja on $($KasutajaNimi)"
+#kasutaja loomise command
+New-Localuser $Kasutajanimi -Password $Kasutajaparool -FullName $Taisnimi -Description $Kirjeldus
