093126a9c43ddca079bf53f60a8ed2689af9f471
kregortoimetaja <kregor.toimetaja@voco.ee>
2022-11-16 06:57:35 -0800

    lokaalse kasutaja kustutamine systeemist.ps1

---
+++ lokaalse kasutaja kustutamine systeemist.ps1
@@ -0,0 +1,22 @@
+﻿#määrame kasutajale parool
+$Kasutajaparool = ConvertTo-SecureString 'Parool11!' -Asplaintext -force
+#kasutaja andmed
+$Eesnimi = Read-host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perenimi"
+#$kasutajnime sätestamine
+$KasutajaNimi = $Eesnimi.ToLower() + " " + $Perenimi.ToLower()
+#$täisnimi
+$TaisNimi =$Eesnimi + " " + $Perenimi
+#$kontokirjeldus
+#kasutajanime confirmimine? write-outputiga
+Write-Output "kustutatav kasutaja on $($KasutajaNimi)"
+#lylitame systeemi veateate valja
+$ErrorActionPreference = "Silentlycontinue"
+#kasutaja loomise command
+Remove-Localuser $Kasutajanimi
+#kontroll
+if(!$?){
+        Write-Output "Tekkinud probleem kasutaja kustutamisega"
+        }
+#systeemi teaded tagasi sisse
+$ErrorActionPreference = "stop"
